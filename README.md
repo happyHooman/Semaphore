40878e43c7a818f3e53948347b175936b2a3d875
Alexandru Tornea <tornea.alexandru@gmail.com>
2016-02-20 15:47:48 +0200

    update 1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Semaphore
 Semaphore application/ JavaScript
+
+## Preview
+Preview it [here](https://rawgit.com/happyHooman/Semaphore/master/index.html)
--- functions.js
+++ functions.js
@@ -1,9 +1,29 @@
-function lightBrightGreen(){
+function lightOnGreen(){
     var light = document.getElementById("green-light");
     light.style.backgroundColor = "#00ff00";
 }
 
 function lightOffGreen(){
     var light = document.getElementById("green-light");
-    light.style.backgroundColor = "#002000";
+    light.style.backgroundColor = "#003000";
+}
+
+function lightOnYellow(){
+    var light = document.getElementById("yellow-light");
+    light.style.backgroundColor = "#ffff00";
+}
+
+function lightOffYellow(){
+    var light = document.getElementById("yellow-light");
+    light.style.backgroundColor = "#303000";
+}
+
+function lightOnRed(){
+    var light = document.getElementById("red-light");
+    light.style.backgroundColor = "#ff0000";
+}
+
+function lightOffRed(){
+    var light = document.getElementById("red-light");
+    light.style.backgroundColor = "#300000";
 }
