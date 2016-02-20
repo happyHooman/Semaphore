27a70def3f3f6bac1a08e6cbe599390adf1caf47
Alexandru Tornea <tornea.alexandru@gmail.com>
2016-02-20 16:48:13 +0200

    commit
    commit

--- functions.js
+++ functions.js
@@ -1,35 +1,21 @@
-function lightOnGreen(){
-    var light = document.getElementById("green-light");
-    light.style.backgroundColor = "#00ff00";
-}
-
-function lightOffGreen(){
-    var light = document.getElementById("green-light");
-    light.style.backgroundColor = "#003000";
-}
-
-function lightOnYellow(){
-    var light = document.getElementById("yellow-light");
-    light.style.backgroundColor = "#ffff00";
-}
-
-function lightOffYellow(){
-    var light = document.getElementById("yellow-light");
-    light.style.backgroundColor = "#303000";
-}
-
-function lightOnRed(){
-    var light = document.getElementById("red-light");
-    light.style.backgroundColor = "#ff0000";
-}
-
-function lightOffRed(){
-    var light = document.getElementById("red-light");
-    light.style.backgroundColor = "#300000";
-}
-
-function lightOn (color){
-    //var colorId = color + "-light";
+function lightOn(color){
     var light = document.getElementById(color + "-light");
-    light.style.backgroundColor = "#00ff00";
+    switch (color) {
+        case 'green': light.style.backgroundColor = "#00ff00";
+            break;
+        case 'yellow': light.style.backgroundColor = "#ffff00";
+            break;
+        case 'red': light.style.backgroundColor = "#ff0000";
+    }
+}
+
+function lightOff(color){
+    var light = document.getElementById(color + "-light");
+    switch (color) {
+        case 'green': light.style.backgroundColor = "#003000";
+            break;
+        case 'yellow': light.style.backgroundColor = "#303000";
+            break;
+        case 'red': light.style.backgroundColor = "#300000";
+    }
 }
