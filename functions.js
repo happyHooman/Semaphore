c4f2df71384e35fa39573dc58b6deddd1e17cf4f
Alexandru Tornea <tornea.alexandru@gmail.com>
2016-02-20 14:58:38 +0200

    Added files via upload

---
+++ functions.js
@@ -0,0 +1,9 @@
+function lightBrightGreen(){
+    var light = document.getElementById("green-light");
+    light.style.backgroundColor = "#00ff00";
+}
+
+function lightOffGreen(){
+    var light = document.getElementById("green-light");
+    light.style.backgroundColor = "#002000";
+}
