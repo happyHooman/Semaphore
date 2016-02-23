03d9076a27eb824551497860cc4cabd5e99d88e2
Alexandru Tornea <tornea.alexandru@gmail.com>
2016-02-23 11:21:29 +0200

    some fixes

--- functions.js
+++ functions.js
@@ -1,43 +1,52 @@
-var redTimeOut = 10000;
-var greenTimeOut = 10000;
+var redTimeOut = 10;
+var greenTimeOut = 10;
 var needStop = false;
+var secMs = 1000;
 
-function lightOn(color){
+function lightOn(color) {
     var light = document.getElementById(color + "-light");
     switch (color) {
-        case 'green': light.style.backgroundColor = "#00ff00";
+        case 'green':
+            light.style.backgroundColor = "#00ff00";
             break;
-        case 'yellow': light.style.backgroundColor = "#ffff00";
+        case 'yellow':
+            light.style.backgroundColor = "#ffff00";
             break;
-        case 'red': light.style.backgroundColor = "#ff0000";
+        case 'red':
+            light.style.backgroundColor = "#ff0000";
     }
 }
 
-function lightOff(color){
+function lightOff(color) {
     var light = document.getElementById(color + "-light");
     switch (color) {
-        case 'green': light.style.backgroundColor = "#003000";
+        case 'green':
+            light.style.backgroundColor = "#003000";
             break;
-        case 'yellow': light.style.backgroundColor = "#303000";
+        case 'yellow':
+            light.style.backgroundColor = "#303000";
             break;
-        case 'red': light.style.backgroundColor = "#300000";
+        case 'red':
+            light.style.backgroundColor = "#300000";
     }
 }
 
-function intermitent(c){
+function intermitent(c) {
     if (needStop) {
         c = 0;
     }
-    switch(c){
+    switch (c) {
         case 1:
             lightOff('green');
-            setTimeout(function(){
-                intermitent(2)},500);
+            setTimeout(function () {
+                intermitent(2)
+            }, 0.5 * secMs);
             break;
         case 2:
             lightOn('green');
-            setTimeout(function(){
-                intermitent(1)},500);
+            setTimeout(function () {
+                intermitent(1)
+            }, 0.5 * secMs);
             break;
         case 0:
             lightOff('green');
@@ -45,13 +54,15 @@ function intermitent(c){
     }
 }
 
-function countdown(timer, color){
-    var p = document.getElementById(color+"-countdown");
+function countdown(timer, color) {
+    var p = document.getElementById(color + "-countdown");
 
     p.innerHTML = timer;
-    if(timer!=0){
+    if (timer != 0) {
         timer--;
-        setTimeout(function(){countdown(timer, color)},1000)
+        setTimeout(function () {
+            countdown(timer, color)
+        }, secMs)
     } else {
         p.innerHTML = '';
     }
@@ -60,7 +71,7 @@ function countdown(timer, color){
 //TODO no info about colors in js
 //TODO add classes ON and OFF
 
-function startLights(step){
+function startLights(step) {
     /*
      lightOn('red');
      setTimeout(function(){lightOff('red'); lightOn('yellow');},redTimeOut);
@@ -68,46 +79,47 @@ function startLights(step){
      setTimeout(function(){lightOff('green')},redTimeOut+greenTimeOut+3000);
      setTimeout(startLights, redTimeOut+greenTimeOut+3000);
      */
-    switch(step){
+    console.debug('step', step);
+    switch (step) {
         case 1:
             lightOn('red');
-            countdown(redTimeOut/1000+3, 'red');
-            setTimeout(function() {
-                startLights(step)}, redTimeOut);
-            step++;
+            countdown(redTimeOut + 3, 'red');
+            setTimeout(function () {
+                startLights(step + 1)
+            }, redTimeOut * secMs);
             break;
         case 2:
             lightOn('yellow');
-            setTimeout(function(){
-                startLights(step)},3000);
-            step++;
+            setTimeout(function () {
+                startLights(step + 1)
+            }, 3 * secMs);
             break;
         case 3:
             lightOff('red');
             lightOff('yellow');
             lightOn('green');
-            countdown(greenTimeOut/1000, 'green');
-            setTimeout(function(){
-                startLights(step)},greenTimeOut-3000);
-            step++;
+            countdown(greenTimeOut, 'green');
+            setTimeout(function () {
+                startLights(step + 1)
+            }, greenTimeOut * secMs - 3 * secMs);
             break;
         case 4:
-            needStop=false;
+            needStop = false;
             intermitent(1);
-            setTimeout(function(){
-                startLights(step)},3000);
-            step++;
+            setTimeout(function () {
+                startLights(step + 1)
+            }, 3 * secMs);
             break;
         case 5:
-            needStop=true;
+            needStop = true;
             lightOn('yellow');
-            setTimeout(function(){
-                startLights(step)},1000);
-            step++;
+            setTimeout(function () {
+                startLights(step + 1)
+            }, secMs);
             break;
         case 6:
             lightOff('yellow');
-            step=1;
+            step = 1;
             startLights(step);
             break;
     }
